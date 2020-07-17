6935584048900ff98729a5c2ae8831df867b6fb6
dyf901 <1378550520@qq.com>
2020-07-17 17:43:04 +0800

    2020/7/17  17:40:00     派派办公代码备份  任务功能

--- src/main/java/com/jjjt/attendance/dao/TaskDao.java
+++ src/main/java/com/jjjt/attendance/dao/TaskDao.java
@@ -1,4 +1,8 @@
 package com.jjjt.attendance.dao;
 
+import java.util.Map;
+
 public interface TaskDao {
+    //发布任务
+    int InsertTask(Map map);
 }
--- src/main/java/com/jjjt/attendance/service/TaskService.java
+++ src/main/java/com/jjjt/attendance/service/TaskService.java
@@ -1,8 +1,19 @@
 package com.jjjt.attendance.service;
 
 import com.jjjt.attendance.dao.TaskDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class TaskService implements TaskDao {
+    @Autowired
+    private TaskDao taskDao;
+
+    //发布任务
+    @Override
+    public int InsertTask(Map map) {
+        return taskDao.InsertTask(map);
+    }
 }
